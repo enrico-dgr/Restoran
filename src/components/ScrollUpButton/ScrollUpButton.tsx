cbac3a92e7aae90eacd2f9b29ef26a64e44b700b
enrico-dgr <enrico.digrazia98@gmail.com>
2025-01-09 15:14:58 +0100

    Added scroll function with animation frame

--- src/components/ScrollUpButton/ScrollUpButton.tsx
+++ src/components/ScrollUpButton/ScrollUpButton.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import useScrollThreshold from '../../hooks/useScrollThreshold/useScrollThreshold'
 import { faArrowUpLong } from '@fortawesome/free-solid-svg-icons'
-import './ScrollUpButton.scss'
 import { useCallback } from 'react'
+import './ScrollUpButton.scss'
 
 export default function ScrollUpButton() {
   const isScrolled = useScrollThreshold(315)
@@ -10,12 +10,26 @@ export default function ScrollUpButton() {
   const scrollUp = useCallback<
     React.MouseEventHandler<HTMLButtonElement>
   >(() => {
-    window.document.querySelectorAll('html, body').forEach(e => {
-      e.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      })
-    })
+    const initialTop = document.documentElement.scrollTop
+
+    const step = () => {
+      const percentageOfInitialScroll =
+        document.documentElement.scrollTop / initialTop
+
+      const delta =
+        20 *
+        Math.exp(
+          -percentageOfInitialScroll * 25 * (1 - percentageOfInitialScroll)
+        )
+
+      document.documentElement.scrollTop -= delta
+
+      if (document.documentElement.scrollTop > 0) {
+        window.requestAnimationFrame(step)
+      }
+    }
+
+    window.requestAnimationFrame(step)
   }, [])
 
   return (
